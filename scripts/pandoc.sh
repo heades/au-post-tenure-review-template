b84c33e1889eaba6fd6113ad649b2bf946e6becd
Clément Aubert <Clement.Aubert@math.cnrs.fr>
2025-02-06 13:59:29 -0500

    Shell check the script
    Used https://www.shellcheck.net to fix:
    
    - https://www.shellcheck.net/wiki/SC2086
    - https://www.shellcheck.net/wiki/SC2242
    
    Please, test before merging.

--- scripts/pandoc.sh
+++ scripts/pandoc.sh
@@ -8,24 +8,24 @@ md_name="${md_filename%.*}"
 debug_mode=0
 
 if [[ -z $build_dir || -z $layout_dir || -z "$md_file" ]]; then
-    echo "Usage: $0 <build directory> <layout directory> <filename.md>"
-    exit -1
+    echo "Usage: $0 <build directory> <layout directory> <filename.md>">&2
+    exit 1
 fi
 
 if [[ ! -e $md_file  ]]; then
-    echo "Error: $md_file doesn't exist."
-    exit -1
+    echo "Error: $md_file doesn't exist.">&2
+    exit 1
 fi
 
 if [[ ! -e $layout_dir  ]]; then
-    echo "Error: $layout_dir doesn't exist."
-    exit -1
+    echo "Error: $layout_dir doesn't exist.">&2
+    exit 1
 fi
 
 default_layout="$layout_dir/default.tex"
 if [[ ! -e $default_layout  ]]; then
-    echo "Error: The default layout $default_layout doesn't exist."
-    exit -1
+    echo "Error: The default layout $default_layout doesn't exist.">&2
+    exit 1
 fi
 
 layout="$default_layout"
@@ -40,7 +40,7 @@ fi
 
 # Make sure the build directory exists:
 if [[ ! -e $build_dir  ]]; then
-    mkdir $build_dir # Create it if it doesn't
+    mkdir "$build_dir" # Create it if it doesn't
 fi
 
 if [[ "$debug_mode" -eq 1 ]]; then
@@ -48,5 +48,4 @@ if [[ "$debug_mode" -eq 1 ]]; then
 fi
 
 # Strips comments in the form of '!-- comment' before processing:
-sed '/\s*\$--/d' $md_file | pandoc --pdf-engine=xelatex -N --citeproc --template=$layout -o "$build_dir/$md_name.pdf"
-
+sed '/\s*\$--/d' "$md_file" | pandoc --pdf-engine=xelatex -N --citeproc --template="$layout" -o "$build_dir/$md_name.pdf"
